970e3a13d1458ee2415fc2e38d5e175de8b8cbce
Anex12 <anexwalker@gmail.com>
2025-07-08 18:44:10 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@ project = 'fixmesticklogin'
 author = 'Your Name'
 release = '1.0'
 
-extensions = []
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 templates_path = ['_templates']
 exclude_patterns = []
 

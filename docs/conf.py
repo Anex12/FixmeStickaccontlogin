c978be0a1600cbfc4f3581a61b33a60931da7341
Anex12 <anexwalker@gmail.com>
2025-07-08 18:37:35 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,9 +6,7 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_favicon = '_static/favicon.png'
-html_js_files = [
-    'chatbot.js',
-]
+
+
